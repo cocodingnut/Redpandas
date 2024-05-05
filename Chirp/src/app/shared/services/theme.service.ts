5656a3200d894dfa4dc5600590563af61a795e89
jsczzzx <jsczzzx@yahoo.com>
2024-05-05 17:59:56 -0400

    localstorage added

--- Chirp/src/app/shared/services/theme.service.ts
+++ Chirp/src/app/shared/services/theme.service.ts
@@ -1,4 +1,3 @@
-// theme.service.ts
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 
@@ -8,13 +7,21 @@ import { Inject, Injectable } from '@angular/core';
 export class ThemeService {
   private currentTheme = 'lara-light-indigo'; // Default theme
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    // Retrieve the theme from localStorage if it exists
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      this.setTheme(storedTheme);
+    }
+  }
 
   setTheme(theme: string) {
     let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
     if (themeLink) {
       themeLink.href = theme + '.css';
       this.currentTheme = theme;
+      // Store the selected theme in localStorage
+      localStorage.setItem('theme', theme);
     }
   }
 

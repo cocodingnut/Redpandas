d4bb655583dd20ab22eb90ffc4dd2546139a9268
mercuryedge117 <mercucyedge@gmail.com>
2024-05-04 19:37:35 -0500

    Revert "test func, REVERT before merge"
    This reverts commit 9c08ac08a83036be08baaaf601ceaa23f8ccf070.

--- Chirp/src/app/features/admin/components/admin-user-list/admin-user-list.component.ts
+++ Chirp/src/app/features/admin/components/admin-user-list/admin-user-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/core/models/user';
-import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
@@ -24,7 +23,7 @@ export class AdminUserListComponent implements OnInit {
     "phone": 1234567890,
   };
 
-  constructor(private users: UserService, private auth: AuthService) { }
+  constructor(private users: UserService) { }
 
   ngOnInit(): void {
     this.users.getAllData().subscribe((res) => {
@@ -34,7 +33,6 @@ export class AdminUserListComponent implements OnInit {
 
   onAddNew(){
     console.log("gonna add new");
-    this.auth.loginAuth("redpanda@gmail.com", "chirp");
   }
   // TODO: try not display user when click delete
   onSelectUser(index: number){

6a667118b051111caf087e0241dbe2040721a3ec
mercuryedge117 <mercucyedge@gmail.com>
2024-05-06 23:36:48 -0500

    comment for cores

--- Chirp/src/app/core/models/profile.ts
+++ Chirp/src/app/core/models/profile.ts
@@ -1,3 +1,6 @@
+/**
+ * Special form of User interface, used for better data security reason, will not contain password and role data
+ */
 export interface Profile {
     name?: string;
     userName: string;

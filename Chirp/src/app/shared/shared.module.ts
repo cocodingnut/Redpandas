8d7db4dbd58f037c70e54a7545383fdbb28e6854
mercuryedge117 <mercucyedge@gmail.com>
2024-05-07 20:15:16 -0500

    fix broken shared module

--- Chirp/src/app/shared/shared.module.ts
+++ Chirp/src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChirrupListComponent } from '../features/chirrup/components/chirrup-list/chirrup-list.component';
-import { ChirrupCardComponent } from '../features/chirrup/components/chirrup-card/chirrup-card.component';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { GetChirrupsService } from '../shared/services/get-chirrups.service';
 import { FormsModule } from '@angular/forms';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { Truncate1Pipe } from './pipes/truncate1.pipe';
+import { ChirrupListComponent } from './components/chirrup-list/chirrup-list.component';
+import { ChirrupCardComponent } from './components/chirrup-card/chirrup-card.component';
 
 
 @NgModule({
